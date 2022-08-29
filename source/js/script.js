e466f181496b522a07e318e37f460c0f0daa53cf
Alena59 <asdalena59@gmail.com>
2022-08-29 13:05:35 +0300

    поправила недочеты

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
 let navMain = document.querySelector('.main-header__wrapper');
 let navToggle = document.querySelector('.main-header__toggle');
 
-navMain.classList.remove('main-header--nojs');
+navMain.classList.remove('main-header__wrapper--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-header--closed')) {
-    navMain.classList.remove('main-header--closed');
-    navMain.classList.add('main-header--open');
+  if (navMain.classList.contains('main-header__wrapper--closed')) {
+    navMain.classList.remove('main-header__wrapper--closed');
+    navMain.classList.add('main-header__wrapper--open');
   } else {
-    navMain.classList.add('main-header--closed');
-    navMain.classList.remove('main-header--open');
+    navMain.classList.add('main-header__wrapper--closed');
+    navMain.classList.remove('main-header__wrapper--open');
   }
 });
